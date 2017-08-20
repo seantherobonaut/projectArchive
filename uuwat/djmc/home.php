10791096d8da64a69adb59608f872d6ae7866376
Sean Leapley <seantherobonaut@gmail.com>
2017-08-20 10:11:28 -0700

    Save all projects

---
+++ uuwat/djmc/home.php
@@ -0,0 +1,33 @@
+<link rel="stylesheet" type="text/css" href="home.css">
+
+<div class="partition">
+	<div class="module" id="main">
+		<div id="outerNav">
+			<div>
+				<span class="setParentWidth">
+					<h1 class="logo">DJMC</h1>
+					<h6 class="logoSub">"...music to believe in"</h6>								
+				</span>
+			</div>
+			<div style="border:1px solid blue">
+				<span class="setParentWidth" id="navMenu" style="border:1px solid white">
+					<a class="links" href="">Music</a>
+					<a class="links" href="">About</a>
+					<a class="links" href="">Events</a>
+					<a class="links" href="">Photos</a>
+					<a class="links" href="">Legal</a>				
+				</span>
+			</div>
+			<br style="clear:both">
+			<div id="mobileButton">
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+			</div>
+		</div>
+
+ 		<p id="rezzy">Result...</p>
+	</div>
+</div>
